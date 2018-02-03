b4c53f2b677132ebc352c1785a8f2063f27fac2f
Contact_Left <ContactLeft@users.noreply.github.com>
2018-02-03 18:53:17 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt clean
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
